4c66b9294be51ee4845848317a0e6ea46966d16b
CRE4PTAL <blazejgoliszek@gmail.com>
2026-06-30 22:40:05 +0200

    Upgrade: Created better layout for header and my picture

--- js/naglowek.js
+++ js/naglowek.js
@@ -4,19 +4,19 @@ function naglowek(loc){
 
     loc = loc.substring(loc.lastIndexOf('/')+1);
     document.write(
-    '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">' +
-        '<div class="container-fluid">' +
-            '<span class="navbar-brand">Błażej Goliszek</span>' +
-            '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">' +
+    '<nav class="navbar navbar-expand-lg navbar-dark site-nav">' +
+        '<div class="container-fluid site-nav__inner">' +
+            '<span class="navbar-brand site-nav__brand">Błażej Goliszek</span>' +
+            '<button class="navbar-toggler site-nav__toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-label="Przełącz nawigację">' +
                 '<span class="navbar-toggler-icon"></span>' +
             '</button>' +
-            '<div class="collapse navbar-collapse" id="navbarNav">' +
-                '<ul class="navbar-nav">'
+            '<div class="collapse navbar-collapse site-nav__collapse" id="navbarNav">' +
+                '<ul class="navbar-nav site-nav__list">'
                     );
                     for(let i = 0; i < adresy.length; i++){
                         document.write(
-                            '<li class="nav-item">' +
-                            '<a class="nav-link'
+                            '<li class="nav-item site-nav__item">' +
+                            '<a class="nav-link site-nav__link'
                         );
                         if(loc == adresy[i]){
                             document.write(' active"');
